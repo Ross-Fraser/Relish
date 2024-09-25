a839689e96fe215d90b2c7fb54a3a0e7f85735e7
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-09-25 18:06:57 +0100

    create: add 403 and 404 error pages

--- relish/urls.py
+++ relish/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
+handler404 = 'roastery.views.custom_page_not_found'
+handler403 = 'roastery.views.custom_permission_denied_view'
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
