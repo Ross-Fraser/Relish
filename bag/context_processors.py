cade541e587468d6cf14552a9e62a0a818abd7cd
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-09-28 17:29:26 +0100

    create: display all quantity in bag

--- bag/context_processors.py
+++ bag/context_processors.py
@@ -1,8 +1,8 @@
-from bag.models import BagItem
+from .models import BagItem
 
 def bag_items_count(request):
+    total_items = 0
     if request.user.is_authenticated:
-        return {
-            'bag_items_count': BagItem.objects.filter(user=request.user).count()
-        }
-    return {'bag_items_count': 0}
+        bag_items = BagItem.objects.filter(user=request.user)
+        total_items = sum(item.quantity for item in bag_items)
+    return {'bag_items_count': total_items}
