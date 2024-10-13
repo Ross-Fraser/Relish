2de7d9c6fddae068b983e2041d7402c8cc7a19c6
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-10-13 16:38:21 +0100

    create: added stripe overlay

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -34,17 +34,28 @@ if (cardElement) {
 var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
     stripe.confirmCardPayment(clientSecret, {
         payment_method: {
             card: card,
         }
     }).then(function(result) {
         if (result.error) {
-            // Show error in the card-errors element
-            var errorElement = document.getElementById('card-errors');
-            errorElement.textContent = result.error.message;
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            $('#payment-form').fadeToggle(100);
+            $('#loading-overlay').fadeToggle(100);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
         } else {
-            // Payment succeeded, submit the form
             if (result.paymentIntent.status === 'succeeded') {
                 form.submit();
             }
