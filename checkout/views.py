81baefd99175b4395465cd2e58647593ea854a5c
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-10-13 15:49:11 +0100

    create: added checkout success logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,12 +1,13 @@
 import stripe
 from decimal import Decimal
 from django.conf import settings
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from checkout.forms import OrderForm
 from bag.models import BagItem
 from roastery.models import CURRENCY_SYMBOLS
+from checkout.models import Order, OrderLineItem
 
 @login_required
 def checkout(request):
@@ -26,7 +27,6 @@ def checkout(request):
 
     # Set up Stripe payment
     stripe.api_key = settings.STRIPE_SECRET_KEY
-
     amount_in_cents = int(grand_total * 100)
 
     intent = stripe.PaymentIntent.create(
@@ -34,6 +34,46 @@ def checkout(request):
         currency=selected_currency.lower(),
         payment_method_types=['card'],
     )
+
+    if request.method == 'POST':
+        form_data = {
+            'first_name': request.POST.get('first_name', ''),
+            'surname': request.POST.get('surname', ''),
+            'email': request.POST.get('email', ''),
+            'phone_number': request.POST.get('phone_number', ''),
+            'street_address1': request.POST.get('street_address1', ''),
+            'street_address2': request.POST.get('street_address2', ''),
+            'town_or_city': request.POST.get('town_or_city', ''),
+            'county': request.POST.get('county', ''),
+            'postcode': request.POST.get('postcode', ''),
+            'country': request.POST.get('country', ''),          
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            # Save the order
+            order = order_form.save(commit=False)
+            order.stripe_pid = intent.id
+            order.original_bag = bag_items
+            order.save()
+
+            # Create order line items
+            for item in bag_items:
+                order_line_item = OrderLineItem(
+                    order=order,
+                    product=item.product,
+                    quantity=item.quantity,
+                    product_size=item.size,
+                )
+                order_line_item.save()
+
+            # Clear the user's bag
+            BagItem.objects.filter(user=request.user).delete()
+
+            # Redirect to the checkout success page
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+
+        else:
+            messages.error(request, 'There was an error with your form. Please double check your information.')
 
     if request.user.is_authenticated:
         initial_data = {
@@ -55,6 +95,25 @@ def checkout(request):
         'order_form': order_form,
         'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
         'client_secret': intent.client_secret,
+    }
+
+    return render(request, template, context)
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
