bf9848304cef9606eb0d0e506762f18b888a5ad1
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-10-14 18:39:11 +0100

    rework: correct shipping and billing details for stripe and button on checkout_success.html

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -54,28 +54,29 @@ form.addEventListener('submit', function(event) {
             payment_method: {
                 card: card,
                 billing_details: {
-                    name: $.trim(form.full_name.value),
+                    name: $.trim(form.first_name.value) + ' ' + $.trim(form.surname.value),
                     phone: $.trim(form.phone_number.value),
                     email: $.trim(form.email.value),
                     address:{
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
                         city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
+                        postal_code: $.trim(form.postcode.value),
+                        country: $.trim(form.country.value),
                     }
                 }
             },
             shipping: {
-                name: $.trim(form.full_name.value),
+                name: $.trim(form.first_name.value) + ' ' + $.trim(form.surname.value),
                 phone: $.trim(form.phone_number.value),
                 address: {
                     line1: $.trim(form.street_address1.value),
                     line2: $.trim(form.street_address2.value),
                     city: $.trim(form.town_or_city.value),
-                    country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
                     state: $.trim(form.county.value),
+                    postal_code: $.trim(form.postcode.value),
+                    country: $.trim(form.country.value),
                 }
             },
         }).then(function(result) {
