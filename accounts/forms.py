d1b7ce155762dc7d0ba56a15369128294fec8cd1
Ross-Fraser <144271273+Ross-Fraser@users.noreply.github.com>
2024-10-15 19:33:57 +0100

    create: add email to registration and profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,4 +9,10 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
